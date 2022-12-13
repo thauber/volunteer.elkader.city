380f80d66e888bf989aa4dda8c7a2a09c7d446e0
Tony Hauber <thauber@gmail.com>
2022-12-13 15:49:49 -0600

    feat: Added greetings modal to project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import { useState } from "react";
 
-export default function Home() {
-  const [reactiveVariable, setReactiveVarible] = useState(0);
+
+function Modal({isVisible, onClose}) {
+
+  const [name, setName] = useState("No Name");
+
+  const containerStyle = {
+    position: 'fixed',
+    width: '100vw',
+    height: '100vh',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    backgroundColor: '#000000AA',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+  const modalStyle = {
+    padding: 10,
+    backgroundColor: '#FFF',
+    color: "#000",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: 130,
+  }
+
+  return isVisible
+    &&<div style={containerStyle}>
+        <div style={modalStyle}>
+          Hi, {name}!
+          <button onClick={()=>setName("Tony")}>Become Tony</button>
+          <button onClick={()=>setName("Sebastienne")}>Become Sebastienne</button>
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>
+}
+
+export default function Home(props) {
+  const [showModal, setShowModal] = useState(false);
 
   function handleClick() {
-    setReactiveVarible(reactiveVariable+1);
+    setShowModal(!showModal);
   }
 
   function resetClick() {
@@ -12,10 +53,9 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <button onClick={handleClick}>CLICK ME</button>
-      <button onClick={resetClick}>resetClick</button>
-      <p>CLICKED {reactiveVariable} TIMES</p>
+    <div >
+      <button onClick={handleClick}>Toggle Information</button>
+      <Modal isVisible={showModal} />
     </div>
   )
 }
